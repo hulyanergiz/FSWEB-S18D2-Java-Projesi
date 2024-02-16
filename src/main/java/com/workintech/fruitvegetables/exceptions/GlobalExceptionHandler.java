c5fa58e8267917b28a41f617290fc2fe0cdc9c57
Hülya Nergiz <akin.hulya@outlook.com>
2024-02-16 18:07:00 +0300

    adds Slf4j and log errors

--- src/main/java/com/workintech/fruitvegetables/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/workintech/fruitvegetables/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.workintech.fruitvegetables.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,16 +13,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(FoodException foodException){
+        log.error("Food Exception: ",foodException.getMessage());
         ErrorResponse response=new ErrorResponse(foodException.getHttpStatus().value(), foodException.getMessage(), LocalDateTime.now());
         return new ResponseEntity<>(response,foodException.getHttpStatus());
     }
 
     @ExceptionHandler
     public ResponseEntity handleException(MethodArgumentNotValidException exception){
+        log.error("Exception: ",exception.getMessage());
         List errorList=exception.getBindingResult().getFieldErrors().stream()
                 .map(fieldError -> {
                     Map<String,String>
@@ -34,6 +38,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleException(Exception exception){
+        log.error("Exception: ",exception.getMessage());
         ErrorResponse response=new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), exception.getMessage(),LocalDateTime.now());
         return new ResponseEntity<>(response,HttpStatus.INTERNAL_SERVER_ERROR);
     }
