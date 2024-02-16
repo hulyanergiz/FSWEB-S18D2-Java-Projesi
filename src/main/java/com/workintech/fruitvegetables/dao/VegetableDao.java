bed5925f2ff4ce34deb0a1c6f7edc20ed0670e6c
Hülya Nergiz <akin.hulya@outlook.com>
2024-02-16 17:46:33 +0300

    edits vegetableDao

--- src/main/java/com/workintech/fruitvegetables/dao/VegetableDao.java
+++ src/main/java/com/workintech/fruitvegetables/dao/VegetableDao.java
@@ -1,6 +1,5 @@
 package com.workintech.fruitvegetables.dao;
 
-import com.workintech.fruitvegetables.entity.Fruit;
 import com.workintech.fruitvegetables.entity.Vegetable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,13 +9,13 @@ import java.util.List;
 public interface VegetableDao extends JpaRepository<Vegetable,Long> {
 
     @Query("SELECT v FROM Vegetable v ORDER BY v.price DESC")
-    List<Fruit> getByPriceDesc();
+    List<Vegetable> getByPriceDesc();
 
     @Query("SELECT v FROM Vegetable v ORDER BY v.price")
-    List<Fruit> getByPriceAsc();
+    List<Vegetable> getByPriceAsc();
 
     @Query("SELECT v FROM Vegetable v WHERE v.name ILIKE %:parameter%")
-    List<Fruit> searchByParameter(String parameter);
+    List<Vegetable> searchByParameter(String parameter);
 
 
 }
